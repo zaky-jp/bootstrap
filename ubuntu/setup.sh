f825579e87f9716f598bc182fc1b99fd85537949
Rintaro Kanzaki <105104188+zaky-jp@users.noreply.github.com>
2022-05-15 14:13:34 +0900

    Added systemd config for mDNS service

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -5,7 +5,12 @@ set -eu
 # install packages
 sudo apt install \
   neovim \
-  avahi-daemon
+  avahi-daemon \
+  jq
+
+# activate mDNS
+sudo systemctl enable avahi-daemon.service
+sudo systemctl start avahi-daemon.service
 
 # setup timezone
 sudo timedatectl set-timezone Asia/Tokyo
