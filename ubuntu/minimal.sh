a60245b908c3dd0388986ddd1c316bd9d7d4c9c7
Rintaro Kanzaki <105104188+zaky-jp@users.noreply.github.com>
2022-06-04 17:57:19 +0900

    Fixed typos & sudo env errors

--- ubuntu/minimal.sh
+++ ubuntu/minimal.sh
@@ -1,44 +1,50 @@
 #!/bin/bash
-
 set -eu
 
-# defaults
-FROM='server'
-GITHUB_REPO='zaky-jp/playground'
-
-# parse arguments
-while [[ $# -gt 0 ]]; do
-  case $1 in
-    --type)
-      TYPE=$2
-      shift
-      shift
-      ;;
-    --github-repo)
-      GITHUB_REPO=$2
-      shift
-      shift
-      ;;
-  esac
-done
+# config
+SETUP_TYPE=${SETUP_TYPE:-'server'}
+GITHUB_REPO=${GITHUB_REPO:-'zaky-jp/playground'}
+DEBIAN_FRONTEND=${DEBIAN_FRONTEND:-'readline'}
+export DEBIAN_FRONTEND
 
 # install ca-certificates
-echo
 echo "Updating package list..."
-sudo apt-get --option "Acquire::https::Verify-Peer=false" update
+sudo --preserve-env=DEBIAN_FRONTEND apt-get \
+  --option "Acquire::https::Verify-Peer=false" update
+echo
 echo "Installing ca-certificates..."
-sudo apt-get --option "Acquire::https::Verify-Peer=false" install --no-install-recommends ca-certificates
+sudo --preserve-env=DEBIAN_FRONTEND apt-get \
+  --option "Acquire::https::Verify-Peer=false" install \
+  --no-install-recommends ca-certificates
 
 # set mirrors
-GITHUB_REPO_RAW=https://raw.githubusercontent.com/${GITHUB_REPO}/main
+_dir=$(mktemp --directory)
+
+# for 
+function _curl() {
+  echo "Fetching $1"
+  curl \
+    --silent \
+    --show-error \
+    --location \
+    --output-dir ${_dir} \
+    --remote-name \
+    "$@"
+}
+
+GITHUB_REPO_RAW=https://raw.githubusercontent.com/${GITHUB_REPO}/main/ubuntu/etc/apt
+echo
 echo "Fetching apt configs from '${GITHUB_REPO}':"
+
 # assuming jammy setup
-curl -sfL ${GITHUB_REPO_RAW}/ubuntu/etc/apt/sources.list | \
-  sudo tee /etc/apt/sources.list >/dev/null
-curl -sfL ${GITHUB_REPO_RAW}/ubuntu/etc/apt/mirrors.${TYPE}.txt | \
-  sudo tee /etc/apt/mirrors.txt >/dev/null
-curl -sfL ${GITHUB_REPO_RAW}/ubuntu/etc/apt/apt.${TYPE}.conf | \
-  sudo tee /etc/apt/apt.conf >/dev/null
+_curl ${GITHUB_REPO_RAW}/sources.list &&
+  sudo cp ${_dir}/sources.list /etc/apt/sources.list
+
+_curl ${GITHUB_REPO_RAW}/mirrors.${SETUP_TYPE}.txt &&
+  sudo cp ${_dir}/mirrors.${SETUP_TYPE}.txt /etc/apt/mirrors.txt
+
+_curl ${GITHUB_REPO_RAW}/apt.${SETUP_TYPE}.conf && \
+  sudo cp ${_dir}/apt.${SETUP_TYPE}.conf /etc/apt/apt.conf
 
 # even running minimal-ubuntu container we still want to cache apt packages
 if [[ -r /etc/apt/apt.conf.d/docker-clean ]]; then
@@ -48,6 +54,8 @@ fi
 # upgrading to latest package
 echo
 echo "Upgrading apt packages..."
-sudo apt-get update && sudo apt-get upgrade
+sudo --preserve-env=DEBIAN_FRONTEND apt-get update
+sudo --preserve-env=DEBIAN_FRONTEND apt-get upgrade
+echo
 echo "Upgrading snap installs..."
 sudo snap refresh
