05e790f7ebefb3bd16e1a801ab12f3e77b412eec
Rintaro Kanzaki <105104188+zaky-jp@users.noreply.github.com>
2022-07-17 00:23:55 +0900

    Enable true color inside tmux using any 256 terminal

--- byobu/install.sh
+++ byobu/install.sh
@@ -1,22 +1,28 @@
-#!/bin/bash
-
+#!/usr/bin/env zsh
 set -eu
 
 # config
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
-SCRIPT_DIR=$(dirname $(realpath -s $0))
+SCRIPT_DIR="$(dirname "$(realpath -s "$0")")"
 
-if [[ $(lsb_release --id --short) = 'Ubuntu' ]]; then
-  sudo apt-get update && \
-    sudo apt-get install byobu
+if (( $+commands[lsb_release] )); then # if linux flavour
+  case "$(lsb_release --id --short)" in
+    Ubuntu)
+      sudo apt-get update && \
+      sudo apt-get install byobu
+      ;;
+  esac
+elif (( $+commands[brew] )) && [[ "$(sw_vers -productName)" == 'macOS' ]]; then
+  if ! (brew list --formulae -1 | grep -q 'byobu'); then
+    brew install byobu
+  fi
 fi
 
 # make sure to prepare config directory
-mkdir -p ${XDG_CONFIG_HOME}/byobu
+mkdir -p "${XDG_CONFIG_HOME}/byobu"
 
 # byobu ocasionally writes directly to status and symlink will break
 # thus not attempting symlinking
-cp ./status ${XDG_CONFIG_HOME}/byobu/status
+cp ./status "${XDG_CONFIG_HOME}/byobu/status"
 
-ln -s ${SCRIPT_DIR}/tmux.conf ${XDG_CONFIG_HOME}/byobu/.tmux.conf
-
+ln -s "${SCRIPT_DIR}/tmux.conf" "${XDG_CONFIG_HOME}/byobu/.tmux.conf"
