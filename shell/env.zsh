ccb8e66c136aa54db296a6d7038d88f5c91c2bb8
Rintaro Kanzaki <105104188+zaky-jp@users.noreply.github.com>
2022-08-13 01:51:49 +0900

    Add zsh config files

---
+++ shell/env.zsh
@@ -0,0 +1,64 @@
+# define local functions
+function append_path {
+  export PATH="$*:${PATH}"
+  typeset -gU PATH path
+}
+
+# declare RUNOS variable
+case $OSTYPE in
+  "linux-gnu")
+    RUNOS="${(L)$(lsb_release --id --short)}" # convert to lowercase
+    ;;
+  "darwin"*)
+    RUNOS='darwin'
+    ;;
+  *)
+    RUNOS="${OSTYPE}"
+    ;;
+esac
+export RUNOS
+
+# configure path
+if [[ ${RUNOS} == "darwin" ]]; then
+  local HOMEBREW_DIR
+  if [[ -x "/opt/homebrew/bin/brew" ]]; then
+    HOMEBREW_DIR="/opt/homebrew"
+  elif [[ -x "/usr/local/bin/brew" ]]; then
+    HOMEBREW_DIR="/usr/local"
+  fi
+
+  if [[ -n "${HOMEBREW_DIR}" ]]; then
+    append_path "${HOMEBREW_DIR}/bin"
+    append_path "${HOMEBREW_DIR}/sbin"
+
+    # program-specific
+    # gcp sdk
+    if [[ -r "${HOMEBREW_DIR}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc" ]]; then
+      source "${HOMEBREW_DIR}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+    fi
+  fi
+fi
+
+if [[ -d "${HOME}/.local/bin" ]]; then
+  # systemd expects user binary in this directory
+  append_path "${HOME}/.local/bin"
+fi
+
+# declare XDG-related variables
+# reuse existing variables if already set by systemd etc.
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-${HOME}/.cache}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}"
+
+# set default editor
+if (( ${+commands[editor]} )); then
+  # fall-back to update-alternatives symlink if avaiable
+  export EDITOR=editor
+elif (( ${+commands[nvim]} )); then
+  export EDITOR=nvim
+else
+  export EDTIOR=vi
+fi
+
+# cleanup
+unfunction append_path
---
+++ shell/rc.zsh
@@ -0,0 +1,79 @@
+#!/usr/bin/env zsh
+
+export SSH_AUTH_SOCK=~/.run/1password/agent.sock
+
+# command history
+export HISTFILE="${XDG_DATA_HOME}/zsh/histfile"
+if [[ ! -w "${HISTFILE}" ]]; then
+  mkdir -p "$(dirname ${HISTFILE})"
+  touch "${HISTFILE}"
+fi
+
+if [[ -w "${HISTFILE}" ]]; then
+  export HISTSIZE=1001
+  export SAVEHIST=1000
+  setopt APPEND_HISTORY # set by default
+  setopt EXTENDED_HISTORY # include timestamp + duration
+  setopt INC_APPEND_HISTORY_TIME # write history after the command is finished
+  setopt HIST_FCNTL_LOCK # use OS-native filelock
+  setopt HIST_EXPIRE_DUPS_FIRST # need $HISTSIZE > $SAVEHIST
+  setopt HIST_IGNORE_DUPS # remove if the same commands are entered consecutively
+  setopt HIST_IGNORE_SPACE # remove first character if whitespace
+  setopt HIST_LEX_WORDS # shell-like whitespace handling
+  setopt HIST_NO_STORE # supress storing history command itself
+  setopt HIST_REDUCE_BLANKS
+  setopt HIST_VERIFY # safe paste; do not execute the command directly
+  setopt HIST_FIND_NO_DUPS
+fi
+
+# ls
+local _ls_args="-aAF"
+local _ls_colour
+# colourize
+case "${RUNOS}" in;
+  'darwin') _ls_colour="-G";;
+  *) _ls_colour="--color";; # assume gnu-ls
+esac
+alias ls="ls ${_ls_args} ${_ls_colour}"
+alias lsl="ls -l ${_ls_args} ${_ls_colour}"
+
+# brew
+if [[ "$RUNOS" == 'darwin' ]]; then
+  # execute command as _brew user
+  alias brew='sudo -i -u _brew -- brew'
+fi
+
+# completion
+autoload -Uz compinit && compinit
+[[ ${ZDOTDIR}/.zcompdump.zwc -nt ${ZDOTDIR}/.zcompdump ]] || zcompile -R -- ${ZDOTDIR}/.zcompdump{.zwc,}
+
+# prepare zplug
+export ZPLUG_HOME="${XDG_DATA_HOME}/zsh/zplug"
+if [[ -d "${ZPLUG_HOME}" ]]; then
+  export ZPLUG_CACHE_DIR="${XDG_CACHE_HOME}/zsh/zplug"
+  source "${ZPLUG_HOME}/init.zsh"
+
+  # list plugins
+  zplug "zplug/zplug", depth:1, hook-build:"zplug --self-manage"
+  zplug "chrissicool/zsh-256color"
+  zplug "zsh-users/zsh-syntax-highlighting", depth:1 # no need to worry about compinit zsh >5.8
+  zplug "zsh-users/zsh-autosuggestions", depth:1 \
+    && export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+  zplug "plugins/shrink-path", from:oh-my-zsh, depth:1
+
+  # make sure to install plugin
+  zplug check || zplug install
+
+  # create cache files if needed
+  () {
+  emulate -L zsh -o extended_glob
+  local f
+  for f in \
+    ${ZPLUG_REPOS}/**/*.zsh(.) \
+    ${ZPLUG_REPOS}/zplug/zplug/autoload/**/^*.zwc(.); do
+      [[ $f.zwc -nt $f ]] || zcompile -R -- $f.zwc $f
+    done
+  }
+
+  zplug load
+fi
