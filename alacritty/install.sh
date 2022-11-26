f3dc5ce4381407e58800ba53b7ca612450862d65
Rintaro Kanzaki <105104188+zaky-jp@users.noreply.github.com>
2022-11-26 12:07:55 +0900

    Added neccessary path

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -11,4 +11,9 @@ SCRIPT_DIR=$(dirname $(realpath -s $0))
 ###########
 # Create symbolic link
 ###########
+
+if [[ ! -d "$ALACRITTY_CONFIG_HOME" ]]; then
+	mkdir -p "$ALACRITTY_CONFIG_HOME"
+fi
+
 ln -s "${SCRIPT_DIR}/configuration.yml" "${ALACRITTY_CONFIG_HOME}/alacritty.yml" 
