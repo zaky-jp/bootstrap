0ad2122b8a9f502acb099451e3f0d6fcf95e1b94
Rintaro Kanzaki <105104188+zaky-jp@users.noreply.github.com>
2023-09-04 22:55:26 +0900

    fix(neovim): sparcecheckout.sh automatically updates submodule

--- neovim/sparcecheckout.sh
+++ neovim/sparcecheckout.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -eux
+
+# initialize jetpack
+git submodule update --init --remote "${PLAYGROUND_DIR}/neovim/jetpack"
+
 # adopted from https://leico.github.io/TechnicalNote/Git/sparse-checkout-submodule
 git -C "${PLAYGROUND_DIR}/neovim/jetpack" config core.sparsecheckout true
+
 cat <<EOS | tee "${PLAYGROUND_DIR}/.git/modules/neovim/jetpack/info/sparse-checkout" >/dev/null
 plugin/jetpack.vim
 EOS
-git -C "${PLAYGROUND_DIR}/neovim/jetpack" read-tree -mu HEAD 
+
+git -C "${PLAYGROUND_DIR}/neovim/jetpack" read-tree -mu HEAD
