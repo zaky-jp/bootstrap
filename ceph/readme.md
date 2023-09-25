a2d91c4b68c0b44c0094353da6cb7b4f7a9501d7
Rintaro Kanzaki <105104188+zaky-jp@users.noreply.github.com>
2023-09-25 23:55:54 +0900

    feat(ceph): whiptail disks and auto-add disks

--- ceph/readme.md
+++ ceph/readme.md
@@ -3,18 +3,13 @@
 - lxd
 - unpartitioned disks (may need to `fdisk` and remove partition if label is already set)
 
-
 ## Instruction
 First create lxd container using ```${PLAYGROUND_DIR}/ceph/spawn.zsh```. This will set up a container with single node ceph.
 
 ### Dashboard password
-Dashboard password needs to be manually set. Create temporary password using below command *inside container*:
-```
-password=$(openssl rand -base64 12 | fold -w 10 | head -1)
-echo $password > /tmp/password.txt
-ceph dashboard ac-user-create --pwd_update_required ceph_admin -i /tmp/password.txt administrator
-```
-Then, access ```ip a``` address at port 8080.
+Dashboard password needs to be updated. Temporary one-off password is displayed on shell.
 
 ### Adding disks
-Follow [official guide](https://github.com/canonical/microceph#%EF%B8%8F-adding-osds-and-rgw) to add disks.
+`spawn.zsh` tries to add disks made available to container via whiptail.
+It anyway asks for confirmation before it wipes disk.
+Follow [official guide](https://github.com/canonical/microceph#%EF%B8%8F-adding-osds-and-rgw) for further guidance.
--- ceph/spawn.zsh
+++ ceph/spawn.zsh
@@ -10,21 +10,46 @@ fi
 source "${PLAYGROUND_DIR}/common/zsh-functions/init"
 
 ## 1. initialisation
-## outcome: $LXD_PROFILE and $CONTAINER_NAME, func run() defined
-local LXD_PROFILE='default'
-local CONTAINER_NAME='microceph'
+## outcome: env vars and local vars initialised, func run() defined
+DEFAULT_PROFILE="${DEFAULT_PROFILE:-default}"
+CONTAINER_NAME="${CONTAINER_NAME:-microceph}"
+CEPH_PROFILE="${CEPH_PROFILE:-microceph}"
+local password=$(openssl rand -base64 12 | fold -w 10 | head -1)
+local passfile="/var/snap/microceph/current/conf/password.txt"
+typeset -a target_devs
+target_devs=($(
+  whiptail --noitem --nocancel --separate-output\
+  --checklist "select disks to be managed by ceph" 15 40 5\
+  $(lsblk -J | jq -rj '.blockdevices | .[] | select(.type == "disk") | " " + .name + " OFF"')\
+  3>&1 1>&2 2>&3
+))
+
 function run(){
   lxc exec "${CONTAINER_NAME}" -- "$@"
 }
 
 ## 2. create and start container
 ## outcome: container created and ready to use
-log_notice "Creating microceph container..."
-if (lxc list -f json | jq -e '.[].name | in({'${CONTAINER_NAME}':null})' >/dev/null); then
+log_notice "Preparing microceph container..."
+if (lxc profile list -f json | jq -r '[.[].name] | contains(["env.CEPH_PROFILE"])' >/dev/null) then
+  log_info "LXD profile '${CEPH_PROFILE}' already exists."
+else
+  log_info "Creating LXD profile '${CEPH_PROFILE}'"
+  lxc profile create microceph
+  for d in ${target_devs}; do
+    log_info "Adding /dev/${d} to LXD profile"
+    lxc profile device add "${CEPH_PROFILE}" $d unix-block path=/dev/$d
+  done
+  lxc profile set "${CEPH_PROFILE}" security.syscalls.intercept.mount true
+fi
+
+if (lxc list -f json | jq -e '.[].name | in({'"${CONTAINER_NAME}"':null})' >/dev/null); then
   log_info "${CONTAINER_NAME} already exists."
 else
+  log_info "Creating microceph container"
   lxc init \
-  --profile "${LXD_PROFILE}" \
+  --profile "${DEFAULT_PROFILE}" \
+  --profile "${CEPH_PROFILE}" \
   ubuntu-minimal:jammy \
   "${CONTAINER_NAME}"
 fi
@@ -50,16 +75,32 @@ fi
 
 ## 4. configure microceph
 ## outcome: microceph cluster initiated, osd rule modified to suit a single instance
+# cluster config
 if (run microceph status 1>/dev/null 2>&1); then
   log_info "microceph has been initialised."
 else
   log_info "boostrapping microceph cluster"
   run microceph cluster bootstrap
-  log_info "setting up dashboard module"
-  run ceph config set mgr mgr/dashboard/ssl false
-  run ceph mgr module enable dashboard
   log_info "changing osd crush rule to suit a single instance"
   run ceph osd crush rule rm replicated_rule
   run ceph osd crush rule create-replicated single default osd
+  log_info "wait for configuration to propagate"
+  sleep 3 # TODO; understand what to wait for
+  log_info "setting up dashboard module"
+  run ceph mgr module enable dashboard
+  run ceph config set mgr mgr/dashboard/ssl false
+  log_info "one-off password has been set as: ${password}"
+  echo "$password" | run tee "$passfile" >/dev/null
+  run ceph dashboard ac-user-create --pwd_update_required ceph_admin -i "$passfile" administrator >/dev/null
+  run ceph mgr module disable dashboard # need to restart dashboard mod
+  run ceph mgr module enable dashboard
 fi
-log_info "refer to readme.md for further instructions..."
+# adding disks
+log_info "adding blockdevices to ceph"
+for d in ${target_devs}; do
+  if (read -q "?Confirm wiping /dev/${d} [y/n]"); then
+    run microceph disk add --wipe /dev/${d}
+  fi
+done
+log_info "enabling rgw"
+microceph enable rgw
