516eccad465e0e29c74cea07053bd490716ac2cb
Rintaro Kanzaki <105104188+zaky-jp@users.noreply.github.com>
2024-03-25 00:36:13 +0900

    fix: skip source unless present

--- macOS/brew/.env.zsh
+++ macOS/brew/.env.zsh
@@ -1,13 +1,2 @@
-# @define environment variables
-typeset -xA brew_config
-brew_config[env.zsh]="${PLAYGROUND_DIR}/brew/.env.zsh"
-brew_config[rc.zsh]="${PLAYGROUND_DIR}/brew/.rc.zsh"
-
-typeset -xA brew_cache
-brew_cache[formulae]="${XDG_CACHE_HOME}/brew/formulae.zsh"
-brew_cache[casks]="${XDG_CACHE_HOME}/brew/casks.zsh"
-# @end
-
-# @run
-source "$brew_cache[formulae]"
-source "$brew_cache[casks]"
+zsh_lazy.push 'formulae' "${XDG_CACHE_HOME}/brew/formulae.zsh"
+zsh_lazy.push 'casks' "${XDG_CACHE_HOME}/brew/casks.zsh"
