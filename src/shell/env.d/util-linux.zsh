408a1dd3dcbdb915b5cbcd251ae7e4e0299691d2
Rintaro Kanzaki <105104188+zaky-jp@users.noreply.github.com>
2024-09-07 12:42:01 +0900

    feat(zsh): changed keybinds, added integration with utli-linux tools (#19)

---
+++ src/shell/env.d/util-linux.zsh
@@ -0,0 +1,3 @@
+if [[ -d "{HOMEBREW_PREFIX}/opt/util-linux" ]]; then
+	unshift path "{HOMEBREW_PREFIX}/opt/util-linux/bin"
+fi
--- src/shell/rc.zsh
+++ src/shell/rc.zsh
@@ -3,6 +3,9 @@ if [[ -r "${XDG_CACHE_HOME}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
+# enable vim-like keybindings
+bindkey -v
+
 # Source rc configuration fragments
 # bootstrap should choose to copy or not copy app-specific rc configs to $XDG_CONFIG_HOME/zsh/rc.d/
 # thus, sourcing all files matching the pattern.
@@ -12,6 +15,12 @@ fi
 		source "${f}"
 	done
 }
+
+# override default keybindings provided by zsh-autocomplete
+bindkey              '^I' menu-select
+bindkey "$terminfo[kcbt]" menu-select
+bindkey -M menuselect              '^I'         menu-complete
+bindkey -M menuselect "$terminfo[kcbt]" reverse-menu-complete
 
 # Compile compdump for faster completion
 #update-compdump
